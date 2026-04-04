d440fd5d092800e2686c909b25b495d27457d58f
watcher041 <watcher041@gmail.com>
2026-04-04 21:35:36 +0900

    追記

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-11770b72.js',
+                path_to_root + 'searchindex-51a72302.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
